5d7cad9b3f0d43ee8b8f09d7a59ee68b16fbe1bb
Deepak Ramani <deepakramani65@gmail.com>
2024-07-24 12:31:44 +0530

    connection update to pool

--- routes/pool.js
+++ routes/pool.js
@@ -1,9 +1,14 @@
 var mysql = require('mysql')
-var pool = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123',
-    database: 'medbazzar',
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12721637',
+    password: 'Resn3QTtKJ',
+    database: 'sql12721637',
     multipleStatements: true,
+    port: 3306
 })
 module.exports = pool;
+
+
+//jdbc:mysql://sql12.freesqldatabase.com:3306/sql12721637
