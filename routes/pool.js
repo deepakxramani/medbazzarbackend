4b42c0fcee142efb567942fc70660e6b0f204153
Deepak Ramani <deepakramani65@gmail.com>
2024-07-25 13:56:44 +0530

    testing

--- routes/pool.js
+++ routes/pool.js
@@ -6,9 +6,8 @@ var pool = mysql.createPool({
     password: 'Resn3QTtKJ',
     database: 'sql12721637',
     multipleStatements: true,
-    port: 3306
 })
 module.exports = pool;
 
 
-//jdbc:mysql://sql12.freesqldatabase.com:3306/sql12721637
+
