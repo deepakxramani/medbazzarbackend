5d0a8b741811dcf9745b0b3aee8458a2303097d3
Deepak Ramani <deepakramani65@gmail.com>
2024-08-14 21:13:55 +0530

    security updates

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors=require('cors');
+
+
 
 var usersRouter = require('./routes/users');
 var categoryRouter=require('./routes/category');
--- routes/pool.js
+++ routes/pool.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 var mysql = require('mysql')
+
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql-3f5f5d7b-medbazzar.k.aivencloud.com',
-    user: 'avnadmin',
-    password: 'AVNS_mcpfDXjLpYLEWBVcjPj',
-    database: 'defaultdb',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     multipleStatements: true,
-    port: 18107
+    port: process.env.DB_PORT
 })
 module.exports = pool;
 
