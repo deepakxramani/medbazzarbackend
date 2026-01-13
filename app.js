9709e8a46a266349cc22eb7567b04c7c9880f9fd
Deepak <deepakramani65@gmail.com>
2026-01-13 22:09:24 +0530

    made some changes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
